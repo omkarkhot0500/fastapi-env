413fe4e5c786df6046de0775df280577d57a3119
omkarkhot0500 <omkarkhot0500@gmail.com>
2025-08-07 22:36:07 +0530

    dynamic paths

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/")       # Base Path
 
-def index():
+def index():        # Path operation function
     return {"message": "Hello, World!"}
+
+@app.get("/about")    # About Path
+def about():
+    return {"message": "This is the about page!"}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
